0ce0473611ff1dbeb69c22f37694984288062ec5
javierroldan2207 <javierroldan2207@gmail.com>
2025-12-04 11:49:10 +0100

    m

--- src/api.js
+++ src/api.js
@@ -2,20 +2,52 @@ const urlBase = "http://localhost:3000/creatures";
 
 function handleResponse(response){
     if(!response.ok){
-        throw new Error("Error al obtener las criaturas.");
+        throw new Error("Error en la petición.");
     }
     return response.json();
 }
 
 const handleError = (error)=> {
-    console.log("Error al cargar." , error);
+    console.log("Error en la operación:" , error);
     throw error;
 }
 
-export const getAllCreatures = ()=>{
+export const getAllCreatures = () => {
     return fetch(urlBase)
-    .then(handleResponse)
-    .catch(handleError)
+        .then(handleResponse)
+        .catch(handleError);
 }
 
- 
+
+export const createCreature = (creature) => {
+    return fetch(urlBase, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(creature)
+    })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export const updateCreature = (id, creature) => {
+    return fetch(`${urlBase}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(creature)
+    })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+// DELETE
+export const deleteCreature = (id) => {
+    return fetch(`${urlBase}/${id}`, {
+        method: "DELETE"
+    })
+    .then(handleResponse)
+    .catch(handleError);
+}
